a386cc71a311dcc8d1c3421aadd62ced81801e0c
Abhinav <abhinav.kapur1408@gmail.com>
2024-12-19 06:53:35 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import MyComponent from "./components/myComponent";
+import ComponentA from "./components/ComponentA";
+import "./index.css";
+
+export default function App() {
+  return <ComponentA />;
+}
